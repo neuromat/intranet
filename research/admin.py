12bbd68d5a66be7e0faf58449e04cd2c2abe7ce4
Carlos Eduardo Ribas <caduribas@gmail.com>
2014-12-03 16:54:46 -0200

    Updated the "WorkInProgressAdmin" class. Author is showed according with user permission.

--- research/admin.py
+++ research/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from research.models import *
-
+import copy
 
 admin.site.register(PaperStatus)
 admin.site.register(TypeAcademicWork)
+
 
 class PaperAdmin(admin.ModelAdmin):
 
@@ -30,10 +31,32 @@ admin.site.register(AcademicWork, AcademicWorkAdmin)
 
 class WorkInProgressAdmin(admin.ModelAdmin):
 
-    fields = ['author', 'status', 'description']
+    fields = ['status', 'description']
 
-    list_display = ('author', 'status',)
+    list_display = ('author', 'status', 'description',)
 
-    list_display_links = ('author', 'status',)
+    list_display_links = ('author', 'status', 'description',)
+
+    # Shows the research according to the user permission
+    def get_queryset(self, request):
+        qs = super(WorkInProgressAdmin, self).get_queryset(request)
+        # If super-user, show all
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(author=request.user)
+
+    # If not superuser, do not show the author field
+    def get_fieldsets(self, request, obj=None):
+        fieldsets = copy.deepcopy(super(WorkInProgressAdmin, self).get_fieldsets(request, obj))
+        if request.user.is_superuser:
+            fieldsets[0][1]['fields'].insert(0, 'author')
+        return fieldsets
+
+    # If not superuser, set the author as the current user
+    def save_model(self, request, obj, form, change):
+        if not request.user.is_superuser:
+            if not change:
+                obj.author = Investigator.objects.get(user=request.user)
+        obj.save()
 
 admin.site.register(WorkInProgress, WorkInProgressAdmin)
