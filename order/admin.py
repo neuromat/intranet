ae93e0a3a9cc2f677b2064c4956c9b332bb71f36
Carlos Eduardo Ribas <caduribas@gmail.com>
2014-12-08 17:19:45 -0200

    Send email after the creation of a new order

--- order/admin.py
+++ order/admin.py
@@ -44,7 +44,7 @@ class SuperOrder(admin.ModelAdmin):
             subject, from_email, to = 'NIRA - Novo pedido', 'neuromatematica@gmail.com', 'admin-nira@numec.prp.usp.br'
             text_content = 'Um novo pedido foi gerado no sistema NIRA.'
             html_content = '<p>Um novo pedido foi gerado no sistema NIRA. ' \
-                           '<a href="http://localhost:8000/admin/order/order/">Ver pedidos.</a></p>'
+                           '<a href="http://sistema.numec.prp.usp.br/admin/order/order/">Ver pedidos.</a></p>'
             msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
             msg.attach_alternative(html_content, "text/html")
             msg.send()
