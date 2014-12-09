80ecf5127a01f5b534ff0617cbcdd5b6d2d93685
Carlos Eduardo Ribas <caduribas@gmail.com>
2014-12-09 15:33:42 -0200

    Bug fix.

--- member/middleware.py
+++ member/middleware.py
@@ -5,9 +5,9 @@ import re
 
 class PasswordChangeMiddleware:
     def process_request(self, request):
-        if request.user.is_authenticated() and re.match(r'^/admin/?', request.path) and \
+        if request.user.is_authenticated() and not request.user.is_superuser and \
                 not re.match(r'^/admin/password_change/?', request.path) and \
-                not re.match(r'^/logout/?', request.path) and not request.user.is_superuser:
+                not re.match(r'^/logout/?', request.path):
 
             profile = request.user.investigator
             if profile.force_password_change:
