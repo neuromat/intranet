56929f478fd9ed79970b562491a8eb4327468197
Carlos Eduardo Ribas <caduribas@gmail.com>
2014-12-12 10:57:46 -0200

    Admin is the default page

--- sistema/urls.py
+++ sistema/urls.py
@@ -4,6 +4,7 @@ from django.utils.translation import ugettext_lazy as _
 
 urlpatterns = patterns(
     '',
+    url(r'^', include(admin.site.urls)),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^i18n/', include('django.conf.urls.i18n')),
     url(r'^cep/', include('cep.urls')),
