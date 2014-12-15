b61c63bdf2de2adfe3c6968a4c9a2fe3447a8a96
Carlos Eduardo Ribas <caduribas@gmail.com>
2014-12-15 17:57:17 -0200

    Set permissions according to is_almost_superuser field

--- order/admin.py
+++ order/admin.py
@@ -4,36 +4,40 @@ from forms import *
 from django.utils.translation import ugettext_lazy as _
 import copy
 from django.core.mail import EmailMultiAlternatives
+from django.contrib.auth.models import User
 
 # Register your models here.
 
 
+def nira_admin():
+    return User.objects.filter(investigator__is_almost_superuser=True)
+
+
 class SuperOrder(admin.ModelAdmin):
-    # Shows the requests according to the user permission
+    # Shows the requests according to the user permission. Users defined as is_almost_superuser can see all orders
     def get_queryset(self, request):
         qs = super(SuperOrder, self).get_queryset(request)
-        # If super-user, show all
-        if request.user.is_superuser:
+        if request.user in nira_admin():
             return qs
         return qs.filter(requester=request.user)
 
-    # If not superuser, do not show the status field
+    # If the user has the is_almost_superuser field disabled, do not show the status field
     def get_readonly_fields(self, request, obj=None):
         ro_fields = super(SuperOrder, self).get_readonly_fields(request, obj)
-        if not request.user.is_superuser:
+        if request.user not in nira_admin():
             ro_fields = list(ro_fields) + ['status']
         return ro_fields
 
-    # If not superuser, do not show the requester field
+    # If the user has the is_almost_superuser field disabled, do not show the requester field
     def get_fieldsets(self, request, obj=None):
         fieldsets = copy.deepcopy(super(SuperOrder, self).get_fieldsets(request, obj))
-        if request.user.is_superuser:
+        if request.user in nira_admin():
             fieldsets[0][1]['fields'].append('requester')
         return fieldsets
 
-    # If not superuser, set the requester as the current user and status as Open
+    # If the user has the is_almost_superuser field disabled, set the requester as the current user and status as Open
     def save_model(self, request, obj, form, change):
-        if not request.user.is_superuser:
+        if request.user not in nira_admin():
             if not change:
                 obj.requester = Investigator.objects.get(user=request.user)
                 obj.status = 'o'
