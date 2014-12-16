b87dbb908f7e884d28f8b009bf2b616f4f94a8ec
Carlos Eduardo Ribas <caduribas@gmail.com>
2014-12-16 11:15:23 -0200

    Update to check if user is NIRA admin

--- research/admin.py
+++ research/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from research.models import *
 import copy
+from django.contrib.auth.models import User
 
 admin.site.register(PaperStatus)
 admin.site.register(TypeAcademicWork)
+
+
+def nira_admin():
+    return User.objects.filter(investigator__is_nira_admin=True)
 
 
 class PaperAdmin(admin.ModelAdmin):
@@ -40,21 +45,21 @@ class WorkInProgressAdmin(admin.ModelAdmin):
     # Shows the research according to the user permission
     def get_queryset(self, request):
         qs = super(WorkInProgressAdmin, self).get_queryset(request)
-        # If super-user, show all
-        if request.user.is_superuser:
+        # If NIRA Admin, show all
+        if request.user in nira_admin():
             return qs
         return qs.filter(author=request.user)
 
-    # If not superuser, do not show the author field
+    # If not NIRA Admin, do not show the author field
     def get_fieldsets(self, request, obj=None):
         fieldsets = copy.deepcopy(super(WorkInProgressAdmin, self).get_fieldsets(request, obj))
-        if request.user.is_superuser:
+        if request.user in nira_admin():
             fieldsets[0][1]['fields'].insert(0, 'author')
         return fieldsets
 
-    # If not superuser, set the author as the current user
+    # If not NIRA Admin, set the author as the current user
     def save_model(self, request, obj, form, change):
-        if not request.user.is_superuser:
+        if not request.user in nira_admin():
             if not change:
                 obj.author = Investigator.objects.get(user=request.user)
         obj.save()
