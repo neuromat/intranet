3fb57c14c70bb217f5abecfc892bef10af969a48
Carlos Eduardo Ribas <caduribas@gmail.com>
2015-03-09 18:29:07 -0300

    Hide fields if "One Way" is selected

--- static/js/order_ticket.js
+++ static/js/order_ticket.js
@@ -1,8 +1,4 @@
 $(document).ready(function () {
-    if (!id_type_0.checked && !id_type_1.checked){
-        $("#id_institution").parents('.row').hide();
-        $("#id_department").parents('.row').hide();
-    }
     $("#id_type_1").click(function () {
         $("#id_inbound_date").parents('.row').hide();
         $("#id_inbound_date_preference").parents('.row').hide();
@@ -11,4 +7,8 @@ $(document).ready(function () {
         $("#id_inbound_date").parents('.row').show();
         $("#id_inbound_date_preference").parents('.row').show();
     });
+    if (id_type_1.checked){
+        $("#id_inbound_date").parents('.row').hide();
+        $("#id_inbound_date_preference").parents('.row').hide();
+    }
 });
