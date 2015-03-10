27f0e6195973fcb000d2193c5dd3441654211210
Carlos Eduardo Ribas <caduribas@gmail.com>
2015-03-10 15:00:55 -0300

    Bug fix. Added lines to allow the visualization of files uploaded

--- sistema/urls.py
+++ sistema/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.utils.translation import ugettext_lazy as _
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = patterns(
     '',
@@ -9,7 +11,7 @@ urlpatterns = patterns(
     url(r'^i18n/', include('django.conf.urls.i18n')),
     url(r'^cep/', include('cep.urls')),
     url(r'^order/', include('order.urls')),
-)
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 admin.site.site_header = _('NeuroMat Individual Report of Activities')
 admin.site.site_title = _('NIRA admin')
