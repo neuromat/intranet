258915315ac651cfaf09d2d966199c9eac84acba
Carlos Eduardo Ribas <caduribas@gmail.com>
2015-03-10 18:03:31 -0300

    Added URLs to show institutes and departments

--- order/urls.py
+++ order/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import patterns, url
 from order import views
 
 urlpatterns = patterns('',
+    url(r'show_institute$', views.show_institute, name='show_institute'),
     url(r'show_department$', views.show_department, name='show_department'),
     url(r'^list_order/$', views.list_order_by_type, name='list_order_by_type'),
     url(r'^additional_options/$', views.select_additional_options, name='selected_additional_options'),
