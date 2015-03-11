b2d26cd55a1150315a5640c53661c35ce1e1ce9c
Carlos Eduardo Ribas <caduribas@gmail.com>
2015-03-11 11:03:12 -0300

    Added DepartmentForm class

--- member/forms.py
+++ member/forms.py
@@ -1,7 +1,8 @@
 from django import forms
-from order.models import Investigator
+from order.models import Investigator, University
 from django.forms import Select
 from cep.widgets import CEPInput
+from django.utils.translation import ugettext_lazy as _
 
 
 class InvestigatorForm(forms.ModelForm):
@@ -23,3 +24,20 @@ class InvestigatorForm(forms.ModelForm):
         css = {
             'all': ('/static/bootstrap_form_helpers/dist/css/bootstrap-formhelpers.min.css',)
         }
+
+
+class DepartmentForm(forms.ModelForm):
+    university = forms.ModelChoiceField(University.objects.all(), label=_('University'),
+                                    widget=Select(attrs={'onchange': 'ajax_filter_institutes(this.value);'}))
+
+    def __init__(self, *args, **kwargs):
+        if 'instance' in kwargs and kwargs['instance'] is not None:
+            initial = {'university': kwargs['instance'].institute.university.id}
+            if 'initial' in kwargs and kwargs['initial'] is not None:
+                kwargs['initial'].update(initial)
+            else:
+                kwargs['initial'] = initial
+        super(DepartmentForm, self).__init__(*args, **kwargs)
+
+    class Media:
+        js = ('/static/js/member_department.js',)
