88f4e2d322fdf8c995c4f4d90734d05c4e38466d
Carlos Eduardo Ribas <caduribas@gmail.com>
2015-03-11 11:04:59 -0300

    Creation of this view. Added show_institute function.

--- member/views.py
+++ member/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from member.models import University, Institute
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+import json as simplejson
 
-# Create your views here.
+
+@login_required
+def show_institute(request):
+    if request.method == 'GET':
+        university_id = request.GET.get('university')
+        university = get_object_or_404(University, id=university_id)
+
+        select = Institute.objects.filter(university=university)
+        institute = []
+        for inst in select:
+            institute.append({'pk': inst.id, 'valor': inst.__unicode__()})
+
+        json = simplejson.dumps(institute)
+        return HttpResponse(json, content_type="application/json")
