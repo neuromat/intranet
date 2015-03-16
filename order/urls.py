f701638ca153974d4f16d121357b9a6f6216c5d6
Carlos Eduardo Ribas <caduribas@gmail.com>
2015-03-16 11:54:24 -0300

    Added URL to scientific missions report

--- order/urls.py
+++ order/urls.py
@@ -7,4 +7,5 @@ urlpatterns = patterns('',
     url(r'show_department$', views.show_department, name='show_department'),
     url(r'^list_order/$', views.list_order_by_type, name='list_order_by_type'),
     url(r'^additional_options/$', views.select_additional_options, name='selected_additional_options'),
+    url(r'^scientific_missions/$', views.scientific_missions_report, name='scientific_missions_report'),
 )
