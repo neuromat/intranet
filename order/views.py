dc3a28ce1f1e54613720f2487dca09f38565210d
Carlos Eduardo Ribas <caduribas@gmail.com>
2015-03-24 17:47:38 -0300

    Code updated to use maskedinput

--- order/views.py
+++ order/views.py
@@ -51,8 +51,8 @@ def scientific_missions_report(request):
         start_date = request.POST['start_date']
         if start_date:
             start_day = start_date[0:2]
-            start_month = start_date[2:4]
-            start_year = start_date[4:8]
+            start_month = start_date[3:5]
+            start_year = start_date[6:10]
             start_date = start_year+start_month+start_day+time
             start_date = datetime.datetime.strptime(start_date, "%Y%m%d %H:%M:%S").date()
             start_date -= datetime.timedelta(days=1)
@@ -62,8 +62,8 @@ def scientific_missions_report(request):
         end_date = request.POST['end_date']
         if end_date:
             end_day = end_date[0:2]
-            end_month = end_date[2:4]
-            end_year = end_date[4:8]
+            end_month = end_date[3:5]
+            end_year = end_date[6:10]
             end_date = end_year+end_month+end_day+time
             end_date = datetime.datetime.strptime(end_date, "%Y%m%d %H:%M:%S").date()
             end_date += datetime.timedelta(days=1)
