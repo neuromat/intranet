9913d87d7f0afc7b16d725fe28275051d3cf9d06
Carlos Eduardo Ribas <caduribas@gmail.com>
2015-04-16 16:41:59 -0300

    Removed unnecessary imports

--- research/forms.py
+++ research/forms.py
@@ -1,7 +1,4 @@
 from django import forms
-from research.models import Unpublished
-from django.forms import Select, TextInput
-from django.utils.translation import ugettext_lazy as _
 
 
 class UnpublishedAdminForm(forms.ModelForm):
