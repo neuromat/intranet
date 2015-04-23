d7553278839a1653ff32256c545530959da28b8f
Carlos Eduardo Ribas <caduribas@gmail.com>
2015-04-23 17:30:32 -0300

    Added a def to get the start date and end date

--- activity/views.py
+++ activity/views.py
@@ -4,6 +4,28 @@ from activity.models import ProjectActivities, SeminarType
 import datetime
 from django.contrib import messages
 
+TIME = " 00:00:00"
+
+
+def start_date_typed(start_date):
+    start_day = start_date[0:2]
+    start_month = start_date[3:5]
+    start_year = start_date[6:10]
+    start_date = start_year+start_month+start_day+TIME
+    start_date = datetime.datetime.strptime(start_date, "%Y%m%d %H:%M:%S").date()
+    start_date -= datetime.timedelta(days=1)
+    return start_date
+
+
+def end_date_typed(end_date):
+    end_day = end_date[0:2]
+    end_month = end_date[3:5]
+    end_year = end_date[6:10]
+    end_date = end_year+end_month+end_day+TIME
+    end_date = datetime.datetime.strptime(end_date, "%Y%m%d %H:%M:%S").date()
+    end_date += datetime.timedelta(days=1)
+    return end_date
+
 
 @login_required
 def seminars_report(request):
@@ -11,26 +33,15 @@ def seminars_report(request):
     categories = SeminarType.objects.all()
 
     if request.method == 'POST':
-        time = " 00:00:00"
         start_date = request.POST['start_date']
         if start_date:
-            start_day = start_date[0:2]
-            start_month = start_date[3:5]
-            start_year = start_date[6:10]
-            start_date = start_year+start_month+start_day+time
-            start_date = datetime.datetime.strptime(start_date, "%Y%m%d %H:%M:%S").date()
-            start_date -= datetime.timedelta(days=1)
+            start_date = start_date_typed(start_date)
         else:
             start_date = datetime.datetime.strptime('19700101 00:00:00', '%Y%m%d %H:%M:%S').date()
 
         end_date = request.POST['end_date']
         if end_date:
-            end_day = end_date[0:2]
-            end_month = end_date[3:5]
-            end_year = end_date[6:10]
-            end_date = end_year+end_month+end_day+time
-            end_date = datetime.datetime.strptime(end_date, "%Y%m%d %H:%M:%S").date()
-            end_date += datetime.timedelta(days=1)
+            end_date = end_date_typed(end_date)
         else:
             now_plus_30 = datetime.datetime.now() + datetime.timedelta(days=30)
             now_plus_30 = now_plus_30.strftime("%Y%m%d %H:%M:%S")
