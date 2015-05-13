2fb5f5bd0467fb86a1bb36e96f9c627c168fd368
Carlos Eduardo Ribas <caduribas@gmail.com>
2015-05-13 12:12:51 -0300

    Added def to create report of published articles

--- research/views.py
+++ research/views.py
@@ -1,3 +1,65 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.utils.translation import ugettext_lazy as _
+from django.contrib import messages
+from models import ResearchResult
+import datetime
 
-# Create your views here.
+
+TIME = " 00:00:00"
+
+def start_date_typed(start_date):
+    start_day = start_date[0:2]
+    start_month = start_date[3:5]
+    start_year = start_date[6:10]
+    start_date = start_year+start_month+start_day+TIME
+    start_date = datetime.datetime.strptime(start_date, "%Y%m%d %H:%M:%S").date()
+    start_date -= datetime.timedelta(days=1)
+    return start_date
+
+
+def end_date_typed(end_date):
+    end_day = end_date[0:2]
+    end_month = end_date[3:5]
+    end_year = end_date[6:10]
+    end_date = end_year+end_month+end_day+TIME
+    end_date = datetime.datetime.strptime(end_date, "%Y%m%d %H:%M:%S").date()
+    end_date += datetime.timedelta(days=1)
+    return end_date
+
+
+def now_plus_thirty():
+    date = datetime.datetime.now() + datetime.timedelta(days=30)
+    date = date.strftime("%Y%m%d %H:%M:%S")
+    date = datetime.datetime.strptime(date, '%Y%m%d %H:%M:%S').date()
+    return date
+
+
+@login_required
+def published_articles(request):
+
+    if request.method == 'POST':
+        start_date = request.POST['start_date']
+        if start_date:
+            start_date = start_date_typed(start_date)
+        else:
+            start_date = datetime.datetime.strptime('19700101 00:00:00', '%Y%m%d %H:%M:%S').date()
+
+        end_date = request.POST['end_date']
+        if end_date:
+            end_date = end_date_typed(end_date)
+        else:
+            end_date = now_plus_thirty()
+
+        published = ResearchResult.objects.filter(research_result_type='p', created__gt=start_date,
+                                                  created__lt=end_date).order_by('created')
+
+        if end_date >= start_date:
+            context = {'published': published}
+            return render(request, 'report/research/published_report.html', context)
+        else:
+            messages.error(request, _('End date should be equal or greater than start date.'))
+            return render(request, 'report/research/published.html')
+
+    return render(request, 'report/research/published.html')
+
