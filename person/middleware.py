4287c691240fa35403be44bc936a3b8c3cbf2223
Carlos Eduardo Ribas <caduribas@gmail.com>
2015-06-03 11:27:05 -0300

    Code updated

--- person/middleware.py
+++ person/middleware.py
@@ -9,6 +9,6 @@ class PasswordChangeMiddleware:
                 not re.match(r'^/admin/password_change/?', request.path) and \
                 not re.match(r'^/logout/?', request.path):
 
-            profile = request.user.person
+            profile = request.user
             if profile.force_password_change:
                 return HttpResponseRedirect('/admin/password_change/')
