ac37f43217d77d718d05ae97eda862d30d40f16b
Carlos Eduardo Ribas <caduribas@gmail.com>
2015-06-11 17:15:13 -0300

    Added URLs to enable the reset of the password

--- sistema/urls.py
+++ sistema/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.utils.translation import ugettext_lazy as _
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.auth import views as auth_views
 
 urlpatterns = patterns(
     '',
@@ -10,6 +11,11 @@ urlpatterns = patterns(
     url(r'^admin/', include(admin.site.urls)),
     url(r'^i18n/', include('django.conf.urls.i18n')),
     url(r'^cep/', include('cep.urls')),
+    url(r'^admin/password_reset/$', auth_views.password_reset, name='admin_password_reset'),
+    url(r'^admin/password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>.+)/$', auth_views.password_reset_confirm,
+        name='password_reset_confirm'),
+    url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
     url(r'^order/', include('order.urls')),
     url(r'^activity/', include('activity.urls')),
     url(r'^research/', include('research.urls')),
