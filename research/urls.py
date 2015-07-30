2945ff33f05e7b788a23627c82cc2e133c9c62c5
Carlos Eduardo Ribas <caduribas@gmail.com>
2015-07-30 18:44:35 -0300

    Added url to the report of academic work

--- research/urls.py
+++ research/urls.py
@@ -4,4 +4,5 @@ from research import views
 
 urlpatterns = patterns('',
     url(r'^published/$', views.published_articles, name='published_articles'),
+    url(r'^academic_works/$', views.academic_works, name='academic_works'),
 )
