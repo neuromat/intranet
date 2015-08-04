d4e1935866d2bd58d1be17b711dc3e4116b6744a
Carlos Eduardo Ribas <caduribas@gmail.com>
2015-08-04 10:50:48 -0300

    Changed the URL from published to published_or_accepted

--- research/urls.py
+++ research/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import patterns, url
 from research import views
 
 urlpatterns = patterns('',
-    url(r'^published/$', views.published_articles, name='published_articles'),
+    url(r'^published_or_accepted/$', views.published_articles, name='published_articles'),
     url(r'^academic_works/$', views.academic_works, name='academic_works'),
 )
