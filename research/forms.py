0312ee8d5107f14bb0c967f20a1828e253b29972
Carlos Eduardo Ribas <caduribas@gmail.com>
2015-08-04 18:30:12 -0300

    Added widget to url field

--- research/forms.py
+++ research/forms.py
@@ -1,7 +1,16 @@
 from django import forms
+from models import Unpublished
+from django.forms import TextInput
 
 
 class UnpublishedAdminForm(forms.ModelForm):
+
+    class Meta:
+        model = Unpublished
+        fields = ['url']
+        widgets = {
+            'url': TextInput(attrs={'size': 90, 'placeholder': 'http://example.com'}),
+        }
 
     class Media:
         js = ('/static/js/research.js',)
