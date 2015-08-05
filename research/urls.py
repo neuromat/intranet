52e2bd79f5d04297132b315a6a877084b15e3967
Carlos Eduardo Ribas <caduribas@gmail.com>
2015-08-05 10:13:56 -0300

    Changed the name from published_or_accepted to articles

--- research/urls.py
+++ research/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import patterns, url
 from research import views
 
 urlpatterns = patterns('',
-    url(r'^published_or_accepted/$', views.published_articles, name='published_articles'),
+    url(r'^articles/$', views.articles, name='articles'),
     url(r'^academic_works/$', views.academic_works, name='academic_works'),
 )
