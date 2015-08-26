195e429fa71a1cb51ac971d88507517f8de61a96
Carlos Eduardo Ribas <caduribas@gmail.com>
2015-08-26 11:50:18 -0300

    New url to create the tex file

--- research/urls.py
+++ research/urls.py
@@ -5,5 +5,5 @@ from research import views
 urlpatterns = patterns('',
     url(r'^articles/$', views.articles, name='articles'),
     url(r'^academic_works/$', views.academic_works, name='academic_works'),
-    url(r'^academic_works/(?P<tex>\d)$', views.academic_works, name='academic_works'),
+    url(r'^academic_works_tex/$', views.academic_works_tex, name='academic_works_tex'),
 )
