0ad019135b8163a382e759a20e8eee77d99afc25
Carlos Eduardo Ribas <caduribas@gmail.com>
2015-09-25 16:37:50 -0300

    File to put initial SQL data

---
+++ add_initial_data.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# This file was created based on what is explained in:
+# https://code.djangoproject.com/wiki/InitialSQLDataDiangoORMWay
+
+from os import environ
+environ['DJANGO_SETTINGS_MODULE'] = 'sistema.settings'
+
+from research.models import TypeAcademicWork
+
+# Types of academic work
+type = TypeAcademicWork(name="Postdoc")
+type.save()
+type = TypeAcademicWork(name="PhD")
+type.save()
+type = TypeAcademicWork(name="MSc")
+type.save()
