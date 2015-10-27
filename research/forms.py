1fda7751728ad2acd1fbbbca4c7ae4e7ce0194b8
sueli-rabaca <sueliusp3@gmail.com>
2015-10-27 16:53:02 -0200

    New research app

--- research/forms.py
+++ research/forms.py
@@ -1,19 +1,6 @@
 from django import forms
-from models import Unpublished, Article
+from models import Article
 from django.forms import TextInput
-
-
-class UnpublishedAdminForm(forms.ModelForm):
-
-    class Meta:
-        model = Unpublished
-        fields = ['url']
-        widgets = {
-            'url': TextInput(attrs={'size': 90, 'placeholder': 'http://example.com'}),
-        }
-
-    class Media:
-        js = ('/static/js/research.js',)
 
 
 class ArticleAdminForm(forms.ModelForm):
