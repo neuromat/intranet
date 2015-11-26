52d21d75634f6769b3c916748c9eab9f16e396ee
Carlos Eduardo Ribas <caduribas@gmail.com>
2015-11-26 17:53:03 -0200

    File updated

--- static/js/published.js
+++ static/js/published.js
@@ -3,28 +3,34 @@ $(document).ready(function () {
     var $id_type = $('[id$=type]');
 
     $id_type.each(function() {
-        if($(this).val() == 'j'){
-            $('[id$=journal]').parents('.row').show();
-        } else{
-            $('[id$=journal]').parents('.row').hide();
-        }
-        if($(this).val() == 'e'){
-            $('[id$=event]').parents('.row').show();
-        } else{
+        if($(this).val() == 'p'){
+            $('[id$=periodical]').parents('.row').show();
+            $('[id$=date]').parents('.row').show();
             $('[id$=event]').parents('.row').hide();
+        } else if($(this).val() == 'e'){
+            $('[id$=event]').parents('.row').show();
+            $('[id$=periodical]').parents('.row').hide();
+            $('[id$=date]').parents('.row').hide();
+        } else{
+            $('[id$=periodical]').parents('.row').hide();
+            $('[id$=event]').parents('.row').hide();
+            $('[id$=date]').parents('.row').hide();
         }
     });
 
     $id_type.change(function(){
-        if($(this).val() == 'j'){
-            $('[id$=journal]').parents('.row').show();
-        } else{
-            $('[id$=journal]').parents('.row').hide();
-        }
-        if($(this).val() == 'e'){
-            $('[id$=event]').parents('.row').show();
-        } else{
+        if($(this).val() == 'p'){
+            $('[id$=periodical]').parents('.row').show();
+            $('[id$=date]').parents('.row').show();
             $('[id$=event]').parents('.row').hide();
+        } else if($(this).val() == 'e'){
+            $('[id$=event]').parents('.row').show();
+            $('[id$=periodical]').parents('.row').hide();
+            $('[id$=date]').parents('.row').hide();
+        } else{
+            $('[id$=periodical]').parents('.row').hide();
+            $('[id$=event]').parents('.row').hide();
+            $('[id$=date]').parents('.row').hide();
         }
     });
 });
