77088ea8576ec0c951b9bc940c9410fb117f8d78
lucashelfstein <lucashelfstein@gmail.com>
2016-02-11 11:46:57 -0200

    Django-jenkins final settings

--- sistema/settings.py
+++ sistema/settings.py
@@ -51,12 +51,11 @@ INSTALLED_APPS = (
 # Possible tasks for Jenkins
 
 JENKINS_TASKS = (
-    # deprecated 'django_jenkins.tasks.with_coverage',
     'django_jenkins.tasks.run_pep8',
-    #'django_jenkins.tasks.run_pylint',
-    #'django_jenkins.tasks.run_pyflakes',
-    #'django_jenkins.tasks.run_jslint',
-    #'django_jenkins.tasks.run_csslint',
+    'django_jenkins.tasks.run_pylint',
+    'django_jenkins.tasks.run_pyflakes',
+    'django_jenkins.tasks.run_jslint',
+    'django_jenkins.tasks.run_csslint',
     'django_jenkins.tasks.run_sloccount',
 )
 
