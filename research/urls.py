0711c23c5384bc784d0448c2337900c26c4e6a1e
carlosribas <caduribas@gmail.com>
2016-03-02 14:57:31 -0300

    NIRA-27 - Create a page to show the periodocals and another page to show the events

--- research/urls.py
+++ research/urls.py
@@ -7,5 +7,6 @@ urlpatterns = patterns('',
     url(r'^academic_works/$', views.academic_works, name='academic_works'),
     url(r'^academic_works_tex/$', views.academic_works_tex, name='academic_works_tex'),
     url(r'^import_papers/$', views.import_papers, name='import_papers'),
-    url(r'^add_periodical/$', views.add_periodical, name='add_periodical'),
+    url(r'^add_periodicals/$', views.add_periodicals, name='add_periodicals'),
+    url(r'^add_papers/$', views.add_papers, name='add_papers'),
 )
