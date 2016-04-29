5499678e7721a7cc4864d6dc46354ff431a79940
carlosribas <caduribas@gmail.com>
2016-04-29 12:39:33 -0300

    Dissemination is now using the helper function "date.py"

--- helper_functions/date.py
+++ helper_functions/date.py
@@ -32,6 +32,13 @@ def end_date_typed(end_date):
     return end_date
 
 
+def now_plus_thirty():
+    date = datetime.datetime.now() + datetime.timedelta(days=30)
+    date = date.strftime("%Y%m%d %H:%M:%S")
+    date = datetime.datetime.strptime(date, '%Y%m%d %H:%M:%S').date()
+    return date
+
+
 def now_plus_five_years():
     date = datetime.datetime.now() + datetime.timedelta(days=5*365)
     date = date.strftime("%Y%m%d %H:%M:%S")
