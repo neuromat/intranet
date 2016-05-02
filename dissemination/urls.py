74f21d2308773c2981044f699e91158c48222254
lucashelfstein <lucashelfstein@gmail.com>
2016-05-02 18:56:09 -0300

    NIRA-93 - Generate latex for the dissemination report If the user chooses an internal dissemination, the table has a caption with the chosen internal media outlet.

--- dissemination/urls.py
+++ dissemination/urls.py
@@ -4,4 +4,5 @@ from dissemination import views
 
 urlpatterns = patterns('',
     url(r'^dissemination_report/$', views.dissemination_report, name='dissemination_report'),
+    url(r'^dissemination_tex/$', views.dissemination_tex, name='dissemination_tex'),
 )
--- dissemination/views.py
+++ dissemination/views.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 from helper_functions.date import *
-from django.shortcuts import render
+from helper_functions.latex import *
+from dissemination.models import Dissemination, Internal, InternalMediaOutlet, TYPE_OF_MEDIA
+from django.shortcuts import render, HttpResponse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from dissemination.models import Dissemination, Internal, InternalMediaOutlet, TYPE_OF_MEDIA
 from django.utils.translation import ugettext_lazy as _
+from django.template.loader import render_to_string
 
 
 @login_required
@@ -13,6 +15,7 @@ def dissemination_report(request):
     types = [{'value': type[0], 'display': type[1].encode('utf-8')} for type in TYPE_OF_MEDIA]
     internal_types = InternalMediaOutlet.objects.all()
     internal_types = [{'value': type.id, 'display': type.name} for type in internal_types]
+    internal_type = ''
 
     if request.method == 'POST':
 
@@ -34,17 +37,20 @@ def dissemination_report(request):
 
         if type == 'i':
             internal_type = request.POST['internal_type']
-            disseminations = Internal.objects.filter(media_outlet_id=internal_type, date__gt=start_date,
-                                                     date__lt=end_date).order_by('-date')
+            disseminations = internal_filter(internal_type, start_date, end_date)
             media_name = InternalMediaOutlet.objects.get(id=internal_type).name
 
         else:
-            disseminations = Dissemination.objects.filter(type_of_media='e', date__gt=start_date,
-                                                          date__lt=end_date).order_by('-date')
+            disseminations = external_filter(start_date, end_date)
             media_name = ''
 
         if end_date >= start_date:
-            context = {'disseminations': disseminations, 'type': type, 'media_name': media_name}
+            context = {'start_date': start_date,
+                       'end_date': end_date,
+                       'disseminations': disseminations,
+                       'type': type,
+                       'media_name': media_name,
+                       'internal_type': internal_type}
             return render(request, 'report/dissemination/dissemination_report.html', context)
         else:
             context = {'types': types, 'internal_types': internal_types}
@@ -54,3 +60,39 @@ def dissemination_report(request):
     context = {'types': types, 'internal_types': internal_types}
 
     return render(request, 'report/dissemination/dissemination.html', context)
+
+
+def dissemination_tex(request):
+
+    start_date = request.GET.get('start_date')
+    end_date = request.GET.get('end_date')
+    type = request.GET.get('type')
+    internal_type = request.GET.get('internal_type')
+
+    if type == 'i':
+        disseminations = internal_filter(internal_type, start_date, end_date)
+
+    else:
+        disseminations = external_filter(start_date, end_date)
+
+    context = {'disseminations': disseminations, 'type': type}
+
+    response = render_to_string('report/dissemination/tex/disseminations.tex', context)
+    response = tex_escape(response)
+
+    latex_response = HttpResponse(response, content_type='text/plain')
+    latex_response['Content-Disposition'] = 'attachment; filename="disseminations.tex"'
+
+    return latex_response
+
+
+def internal_filter(internal_type, start_date, end_date):
+    disseminations = Internal.objects.filter(media_outlet_id=internal_type, date__gt=start_date,
+                                                     date__lt=end_date).order_by('-date')
+    return disseminations
+
+
+def external_filter(start_date, end_date):
+    disseminations = Dissemination.objects.filter(type_of_media='e', date__gt=start_date,
+                                                          date__lt=end_date).order_by('-date')
+    return disseminations
