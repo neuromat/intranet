ee0d60eb5ca5b691348f8efd25397b17faa6b136
carlosribas <caduribas@gmail.com>
2016-05-06 10:51:33 -0300

    NIRA-94 - Create report of Scientific Mission (html and latex)

--- scientific_mission/urls.py
+++ scientific_mission/urls.py
@@ -6,4 +6,5 @@ urlpatterns = patterns('',
                        url(r'^load_origin_cities/$', views.load_origin_cities, name='load_origin_cities'),
                        url(r'^load_destination_cities/$', views.load_destination_cities,
                            name='load_destination_cities'),
+                       url(r'^report/$', views.missions_report, name='missions_report'),
                        )
