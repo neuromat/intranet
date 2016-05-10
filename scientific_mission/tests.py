708b895d28458a8b796a2ae592d8bf266c123ed6
lucashelfstein <lucashelfstein@gmail.com>
2016-05-10 12:08:55 -0300

    NIRA-97 - Create unit tests for the Scientific Missions app Unit tests implemented. The following tests are performed: - Scientific Missions report with date; - Scientific Missions report without date; - Load Origin Cities; - Load Destination Cities.

--- scientific_mission/tests.py
+++ scientific_mission/tests.py
@@ -2,36 +2,13 @@ import datetime
 from django.core.urlresolvers import reverse
 from django.test import TestCase
 from cities_light.models import City, Country
-from models import ScientificMission
+from scientific_mission.models import ScientificMission
 from person.models import Person
 from research.tests import system_authentication
 
 
-# Create a scientific mission for testing, with origin and destination already set
-def scientific_mission(person, departure, arrival, amount_paid):
-
-    # Origin country: Brazil
-    # Origin city: Alegrete, Rio Grande do Sul
-    # o_country = Country.objects.filter(id=31)
-    # o_city = City.objects.filter(id=2130)
-
-    # Destination country: United States
-    # Destination city is: Albany, California
-    # d_country = Country.objects.filter(id=234)
-    # d_city = City.objects.filter(id=21470)
-
-    """
-    return ScientificMission(person=person,
-                             origin_country=o_country, origin_city=o_city,
-                             destination_country=d_country, destination_city=d_city,
-                             departure=departure, arrival=arrival, amount_paid=amount_paid)
-    """
-
-    country = Country()
-    country.save()
-    city = City(country=country)
-    city.save()
-
+# Create a scientific mission for testing
+def scientific_mission(person, city, country, departure, arrival, amount_paid):
     return ScientificMission(person=person,
                              origin_country=country, origin_city=city,
                              destination_country=country, destination_city=city,
@@ -40,38 +17,61 @@ def scientific_mission(person, departure, arrival, amount_paid):
 
 class ScientificMissionsTest(TestCase):
 
+    """
+    The following tests are performed:
+    1 - Scientific Missions report with date;
+    2 - Scientific Missions report without date;
+    3 - Load Origin Cities;
+    4 - Load Destination Cities.
+    """
+
     def setUp(self):
         logged, self.user, self.factory = system_authentication(self)
         self.assertEqual(logged, True)
 
+        country = Country(id=31)
+        country.save()
+
+        city = City(country=country)
+        city.save()
+
         person = Person(full_name="Fulano Testeiro")
         person.save()
 
-        departure = datetime.date(2015, 1, 15)
-        arrival = datetime.date(2015, 1, 16)
+        departure1 = datetime.date(2015, 1, 15)
+        arrival1 = datetime.date(2015, 1, 16)
+
+        departure2 = datetime.date(2014, 1, 15)
+        arrival2 = datetime.date(2014, 1, 16)
 
         amount_paid = 666
 
-        mission_example = scientific_mission(person, departure, arrival, amount_paid)
-        mission_example.save()
+        mission1 = scientific_mission(person, city, country, departure1, arrival1, amount_paid)
+        mission1.save()
 
-    def test_select_cities(self):
-        pass
+        mission2 = scientific_mission(person, city, country, departure2, arrival2, amount_paid)
+        mission2.save()
 
-    def test_load_origin_cities(self):
-        pass
+    def test_report(self):
 
-    def test_missions_report_with_date(self):
+        # With date
         response = self.client.post(reverse('missions_report'), {'start_date': '01-01-2015',
                                                                  'end_date': '03-05-2017'})
         cont = response.context['missions']
         self.assertEqual(len(cont), 1)
         self.assertEqual(response.status_code, 200)
 
-    def test_missions_report_without_date(self):
-
+        # Without date
         response = self.client.post(reverse('missions_report'), {'start_date': '',
                                                                  'end_date': ''})
         cont = response.context['missions']
-        self.assertEqual(len(cont), 1)
+        self.assertEqual(len(cont), 2)
+        self.assertEqual(response.status_code, 200)
+
+    def test_load_cities(self):
+
+        response = self.client.get(reverse('load_origin_cities'), {'origin_country': 31})
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.get(reverse('load_destination_cities'), {'destination_country': 31})
         self.assertEqual(response.status_code, 200)
