4d1bcc38ef470df24c91e3781b3e40154150afcb
lucashelfstein <lucashelfstein@gmail.com>
2016-06-09 18:28:41 -0300

    NIRA-74 - With the info from scientific missions, create a PDF file to generate the "Anexo 5" Pendências: formatação, correção e definição de datas, número de processo, viagens com múltiplas escalas.

--- scientific_mission/views.py
+++ scientific_mission/views.py
@@ -1,14 +1,20 @@
+import json as simplejson
 from dal import autocomplete
+from activity.views import render_to_pdf
 from cities_light.models import City
 from helper_functions.date import *
 from scientific_mission.models import ScientificMission
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.http import Http404, HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.utils.translation import ugettext_lazy as _
 from helper_functions.latex import generate_latex
+from person.models import Person
 
 
 class CityAutocomplete(autocomplete.Select2QuerySetView):
+
     def get_queryset(self):
         if not self.request.user.is_authenticated():
             return City.objects.none()
@@ -19,6 +25,54 @@ class CityAutocomplete(autocomplete.Select2QuerySetView):
             qs = qs.filter(name__istartswith=self.q)
 
         return qs
+
+@login_required
+def anexo5(request):
+
+    people = Person.objects.all()
+    missions = ScientificMission.objects.all()
+
+    if request.method == 'POST':
+
+        mission_id = request.POST['title']
+
+        if mission_id is None or mission_id == '':
+            messages.error(request, _('You have to choose a scientific mission!'))
+            context = {'people': people, 'missions': missions}
+            return render(request, 'anexo/anexo5.html', context)
+        else:
+            try:
+                mission = ScientificMission.objects.get(id=mission_id)
+            except ScientificMission.DoesNotExist:
+                raise Http404(_('No scientific mission matches the given query.'))
+
+            return render_to_pdf(
+                'anexo/anexo5_pdf.html',
+                {
+                    'pagesize': 'A4',
+                    'mission': mission,
+                    'person': mission.person,
+                }
+            )
+
+    context = {'people': people, 'missions': missions}
+    return render(request, 'anexo/anexo5.html', context)
+
+
+@login_required
+def mission_show_titles(request):
+    if request.method == 'GET':
+        person_id = request.GET.get('person')
+        person = get_object_or_404(Person, id=person_id)
+
+        missions = ScientificMission.objects.filter(person=person)
+        titles = []
+
+        for title in missions:
+            titles.append({'pk': title.id, 'valor': title.__unicode__()})
+
+        json = simplejson.dumps(titles)
+        return HttpResponse(json, content_type="application/json")
 
 
 @login_required
