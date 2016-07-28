c6f5cdc93c6f1aa4cbef1e903f201022911e6aa6
lucashelfstein <lucashelfstein@gmail.com>
2016-07-28 15:51:18 -0300

    NIRA-111 - Create a template for the Documents that can be generated with NIRA Page documents created without a specific app for it.

--- activity/urls.py
+++ activity/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     url(r'^seminars/$', views.seminars_report, name='seminars_report'),
     url(r'^training_programs/$', views.training_programs_report, name='training_programs_report'),
     url(r'^meetings/$', views.meetings_report, name='meetings_report'),
-    url(r'^seminar_poster/$', views.seminar_poster, name='seminar_poster'),
     url(r'^seminar_show_titles/$', views.seminar_show_titles, name='seminar_show_titles'),
     url(r'^seminar_latex/$', views.seminar_latex, name='seminar_latex'),
     url(r'^training_programs_latex/$', views.training_programs_latex, name='training_programs_latex'),
---
+++ helper_functions/show_page.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.core.urlresolvers import reverse
+from django.shortcuts import render, redirect
+from django.utils.translation import ugettext_lazy as _
+
+
+@login_required
+def display_documents(request):
+    if request.user.has_perm('custom_auth.create_documents'):
+        return render(request, 'documents/documents.html')
+    else:
+        messages.warning(request, _("You don't have permission to access this page."))
+        return redirect(reverse('admin:index'))
