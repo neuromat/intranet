38d24f93edbdbb4e86d664d4169de22340c63ee6
carlosribas <caduribas@gmail.com>
2016-08-02 16:05:57 -0300

    Show content, documents and reports in accordance with the template used

--- helper_functions/show_page.py
+++ helper_functions/show_page.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.core.urlresolvers import reverse
@@ -8,7 +9,10 @@ from django.utils.translation import ugettext_lazy as _
 @login_required
 def display_documents(request):
     if request.user.has_perm('custom_auth.create_documents'):
-        return render(request, 'documents/documents.html')
+        if 'suit' in settings.INSTALLED_APPS:
+            return render(request, 'documents/documents_suit.html')
+        else:
+            return render(request, 'documents/documents.html')
     else:
         messages.warning(request, _("You don't have permission to access this page."))
         return redirect(reverse('admin:index'))
@@ -17,7 +21,10 @@ def display_documents(request):
 @login_required
 def display_reports(request):
     if request.user.has_perm('custom_auth.view_reports'):
-        return render(request, 'report/reports.html')
+        if 'suit' in settings.INSTALLED_APPS:
+            return render(request, 'report/reports_suit.html')
+        else:
+            return render(request, 'report/reports.html')
     else:
         messages.warning(request, _("You don't have permission to access this page."))
         return redirect(reverse('admin:index'))
@@ -26,7 +33,10 @@ def display_reports(request):
 @login_required
 def display_add_content(request):
     if request.user.has_perm('custom_auth.add_content'):
-        return render(request, 'add_content/add_content.html')
+        if 'suit' in settings.INSTALLED_APPS:
+            return render(request, 'add_content/add_content_suit.html')
+        else:
+            return render(request, 'add_content/add_content.html')
     else:
         messages.warning(request, _("You don't have permission to access this page."))
         return redirect(reverse('admin:index'))
