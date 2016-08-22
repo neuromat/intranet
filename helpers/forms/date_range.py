68359aefff6ec629fe3e4fb9d797b157d524aaab
carlosribas <caduribas@gmail.com>
2016-08-22 16:19:22 -0300

    NIRA-118 - Update the use of datepicker

--- helpers/forms/date_range.py
+++ helpers/forms/date_range.py
@@ -1,10 +1,12 @@
 from django import forms
+from django.utils.translation import ugettext_lazy as _
+
 
 #  Form with jquerys datepicker
-DateInput = forms.DateInput(format=['%d-%m-%Y'], attrs={'class': 'datepicker', 'placeholder': 'dd/mm/aaaa'})
+DateInput = forms.DateInput(format=['%d-%m-%Y'], attrs={'class': 'datepicker', 'placeholder': _('dd/mm/yyyy')})
 
 
 #  Creates a form that comes with the datepicker widget
 class DateRangeForm(forms.Form):
-    start_date = forms.DateField(widget=DateInput)
-    end_date = forms.DateField(widget=DateInput)
+    start_date = forms.DateField(widget=DateInput, label=_('Start date'))
+    end_date = forms.DateField(widget=DateInput, label=_('End date'))
