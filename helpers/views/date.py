d815fea4405c2e41770a019ce711d731fc45ed9b
carlosribas <caduribas@gmail.com>
2016-09-29 17:33:06 -0300

    start_date_typed and end_date_typed was removed

--- helpers/views/date.py
+++ helpers/views/date.py
@@ -5,26 +5,6 @@ import datetime
 TIME = " 00:00:00"
 
 
-def start_date_typed(start_date):
-    start_day = start_date[0:2]
-    start_month = start_date[3:5]
-    start_year = start_date[6:10]
-    start_date = start_year+start_month+start_day+TIME
-    start_date = datetime.datetime.strptime(start_date, "%Y%m%d %H:%M:%S").date()
-    start_date -= datetime.timedelta(days=1)
-    return start_date
-
-
-def end_date_typed(end_date):
-    end_day = end_date[0:2]
-    end_month = end_date[3:5]
-    end_year = end_date[6:10]
-    end_date = end_year+end_month+end_day+TIME
-    end_date = datetime.datetime.strptime(end_date, "%Y%m%d %H:%M:%S").date()
-    end_date += datetime.timedelta(days=1)
-    return end_date
-
-
 def now_plus_thirty():
     date = datetime.datetime.now() + datetime.timedelta(days=30)
     date = date.strftime("%Y%m%d %H:%M:%S")
