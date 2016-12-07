384ab18feda5a1ebcc6130b5a20d238adfd05696
lucashelfstein <lucashelfstein@gmail.com>
2016-12-07 14:52:03 -0200

    NIRA-119 - Scientific Missions pdf report view

--- scientific_mission/urls.py
+++ scientific_mission/urls.py
@@ -3,7 +3,11 @@ from django.conf.urls import url
 
 
 urlpatterns = [
-    url(r'^scientific_missions_tex/$', views.missions_tex, name='scientific_missions_tex'),
     url(r'^city_autocomplete/$', views.CityAutocomplete.as_view(), name='city_autocomplete'),
-    url(r'^show_missions/$', views.mission_show_titles, name='anexo_missions')
+    url(r'^show_missions/$', views.mission_show_titles, name='anexo_missions'),
+
+    #  reports
+    url(r'^scientific_missions_tex/$', views.missions_tex, name='scientific_missions_tex'),
+    url(r'^scientific_missions_pdf/$', views.missions_pdf, name='scientific_missions_pdf'),
+
 ]
--- scientific_mission/views.py
+++ scientific_mission/views.py
@@ -224,3 +224,17 @@ def missions_tex(request):
     context = {'missions': missions}
 
     return generate_latex('report/scientific_mission/tex/scientific_missions.tex', context, 'scientific_missions')
+
+
+@login_required
+def missions_pdf(request):
+
+    start_date = request.GET.get('start_date')
+    end_date = request.GET.get('end_date')
+
+    missions = get_missions(datetime.datetime.strptime(start_date, "%Y-%m-%d").date(),
+                            datetime.datetime.strptime(end_date, "%Y-%m-%d").date())
+
+    context = {'start_date': start_date, 'end_date': end_date, 'missions': missions}
+
+    return render_to_pdf('report/scientific_mission/pdf/scientific_missions.html', context)
