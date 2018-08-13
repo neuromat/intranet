754dc38a5fd96c47875051bc42cb18ebf1a6703e
carlosribas <caduribas@gmail.com>
2018-08-13 16:47:57 -0300

    Fixing files after updating the django version

--- sistema/urls.py
+++ sistema/urls.py
@@ -41,7 +41,6 @@ content_patterns = [
 
 urlpatterns = [
     url(r'^', include(admin.site.urls)),
-    url(r'^admin/', include(admin.site.urls)),
     url(r'^i18n/', include('django.conf.urls.i18n')),
     url(r'^admin/password_reset/$', auth_views.password_reset, name='admin_password_reset'),
     url(r'^admin/password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
