1de0fa680145512a6d14062f0a9f9dc91120dac9
lucazweb <lucazwebmail@gmail.com>
2019-09-11 13:50:26 -0300

    Adds migrations and updates settings to include suits and other project specific settings

--- sistema/settings.py
+++ sistema/settings.py
@@ -36,6 +36,7 @@ AUTH_USER_MODEL = 'custom_auth.User'
 
 INSTALLED_APPS = (
     'modeltranslation',
+    'suit',
     'dal',
     'dal_select2',
     'django.contrib.admin',
@@ -67,7 +68,6 @@ MIDDLEWARE = (
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    # 'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
@@ -153,8 +153,53 @@ MEDIA_URL = '/media/'
 
 LOGIN_URL = '/login/'
 
+CEPID_NAME = 'Example CEPID Name'
+
+from django.utils.translation import ugettext_lazy as _
+SUIT_CONFIG = {
+    'ADMIN_NAME': 'NIRA',
+    'MENU_OPEN_FIRST_CHILD': False,
+    'HEADER_DATE_FORMAT': 'l, d F Y',
+    'MENU': (
+        {'app': 'configuration', 'icon': 'icon-cog'},
+        {'app': 'activity', 'icon': 'icon-calendar'},
+        {'app': 'dissemination', 'icon': 'icon-facetime-video'},
+        {'app': 'person', 'icon': 'icon-user'},
+        {'app': 'research', 'icon': 'icon-book'},
+        {'app': 'scientific_mission', 'icon': 'icon-plane'},
+        {'label': _('Reports'), 'url': '/reports/',
+         'icon': 'icon-th', 'permissions': 'custom_auth.view_reports', 'models': (
+            {'label': _('Academic works'), 'url': '/reports/academic_works'},
+            {'label': _('Articles'), 'url': '/reports/articles'},
+            {'label': _('Disseminations'), 'url': '/reports/dissemination'},
+            {'label': _('Meetings'), 'url': '/reports/meetings'},
+            {'label': _('Researchers'), 'url': '/reports/researchers'},
+            {'label': _('Scientific missions'), 'url': '/reports/scientific_mission'},
+            {'label': _('Seminars'), 'url': '/reports/seminars'},
+            {'label': _('Training programs'), 'url': '/reports/training_programs'},
+        )},
+        {'label': _('Add content'), 'url': '/add_content',
+         'icon': 'icon-upload', 'permissions': 'custom_auth.add_content', 'models': (
+            {'label': _('Create/Update citation name'), 'url': '/add_content/citation_names'},
+            {'label': _('Import papers'), 'url': '/add_content/import_papers'},
+        )},
+        {'label': _('Documents'), 'url': '/documents',
+         'icon': 'icon-list-alt', 'permissions': 'custom_auth.create_documents', 'models': (
+            {'label': _('Certificate'), 'url': '/documents/certificate'},
+            {'label': _('FAPESP - appendix 5'), 'url': '/documents/anexo5'},
+            {'label': _('FAPESP - appendix 6'), 'url': '/documents/anexo6'},
+            {'label': _('FAPESP - appendix 7'), 'url': '/documents/anexo7'},
+            {'label': _('FAPESP - appendix 9'), 'url': '/documents/anexo9'},
+            {'label': _('Seminar poster'), 'url': '/documents/seminars_poster'},
+        )},
+        '-',
+        {'app': 'cities_light', 'icon': 'icon-globe', 'label': _('Cities')},
+        {'app': 'custom_auth', 'icon': 'icon-lock', 'label': _('Users')},
+        {'app': 'auth', 'icon': 'icon-lock', 'label': _('Groups')},
+    ),
+}
+
 try:
     from .settings_local import *
 except ImportError:
-    print("ERROR IMPORT")
     pass
