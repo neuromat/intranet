24ebccd74e6ff03213d33efc2047ea3f0a656804
lucazweb <lucazwebmail@gmail.com>
2019-09-13 14:55:34 -0300

    Fixes report input datepicker and mask
    Fixes references for datepicker and input masks in reports inputs.

--- static/js/scientific_mission_report.js
+++ static/js/scientific_mission_report.js
@@ -1,8 +1,14 @@
 $(document).ready(function () {
-    var ua = navigator.userAgent.toLowerCase();
-    var isNotAndroid = ua.indexOf("android") <= -1; //&& ua.indexOf("mobile");
+
+    let ua = navigator.userAgent.toLowerCase();
+    let isNotAndroid = ua.indexOf("android") <= -1; //&& ua.indexOf("mobile");
+
     if (isNotAndroid) {
         $("#id_start_date").mask("99/99/9999");
         $("#id_end_date").mask("99/99/9999");
     }
+
+    $('.report-date-mask').mask("99/99/9999");
+
+
 });
